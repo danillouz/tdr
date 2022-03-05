40dc7c3c77b44de14a44c884d47c75f26620ada1
Daniël Illouz <daniel.illouz@me.com>
2022-03-05 14:32:38 +0100

    Implement DNS message header packing/unpacking

--- internal/dns/header.go
+++ internal/dns/header.go
@@ -1,5 +1,10 @@
 package dns
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 // OpCode represents a DNS operation code.
 type OpCode byte
 
@@ -41,10 +46,8 @@ const (
 	RCodeRefused
 )
 
-// Header represents the DNS message header.
-// It has the following format:
+// Header represents the DNS message header. It has the following format:
 //
-//                                 1  1  1  1  1  1
 //   0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
 // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 // |                      ID                       |
@@ -112,4 +115,132 @@ type Header struct {
 
 	// ARCount specifies the number of resource records in the additional section.
 	ARCount uint16
+}
+
+// Pack encodes the DNS message header fields into binary format.
+func (h *Header) Pack() ([]byte, error) {
+	// The header fields must be packed into 6 sections of 16 bits (big endian),
+	// where each section will be written into a single buffer.
+	buff := new(bytes.Buffer)
+
+	// First section: the ID is 16 bits, so just write it to the buffer.
+	err := binary.Write(buff, binary.BigEndian, h.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Second section: left-shift the bits of each field into the correct
+	// position, and OR to "merge" all bits into a single section s.
+	//
+	//  15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+	// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+	// |QR|   OPCODE  |AA|TC|RD|RA|   Z    |   RCODE   |
+	// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+	var s uint16
+	s |= uint16(h.QR) << 15
+	s |= uint16(h.OpCode) << 11
+	s |= uint16(h.AA) << 10
+	s |= uint16(h.TC) << 9
+	s |= uint16(h.RD) << 8
+	s |= uint16(h.RA) << 7
+	s |= uint16(h.RCode) << 0
+	err = binary.Write(buff, binary.BigEndian, s)
+	if err != nil {
+		return nil, err
+	}
+
+	// Remaining sections: these take up 16 bits each, so just write them to the
+	// buffer.
+	err = binary.Write(buff, binary.BigEndian, h.QDCount)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(buff, binary.BigEndian, h.ANCount)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(buff, binary.BigEndian, h.NSCount)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(buff, binary.BigEndian, h.ARCount)
+	if err != nil {
+		return nil, err
+	}
+
+	return buff.Bytes(), nil
+}
+
+// Unpack decodes the DNS message header field bytes (big-endian; network
+// order). It returns either the unpacked bytes count or an error.
+func (h *Header) Unpack(msg []byte) (int, error) {
+	// The first 2 bytes contain the first section; ID.
+	//
+	// Left-shift the first byte to the "left most" position, and OR it with the
+	// second byte to "merge" it back into a single section of 16 bits.
+	h.ID = uint16(msg[0])<<8 | uint16(msg[1])
+
+	// The 3rd and 4th bytes contain the second section.
+	//
+	//   7  6  5  4  3  2  1  0  7  6  5  4  3  2  1  0
+	// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+	// |QR|   OPCODE  |AA|TC|RD|RA|   Z    |   RCODE   |
+	// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+	//
+	// To "query" the header's bit fields, for each bit field:
+	// - Right-shift the byte to the "right most" position, so only the queried
+	//   bit field remains.
+	// - Create a bit mask where all "left most" bits are "turned off", _except_
+	//   the bit(s) in the queried bit field (mask the length of the bit field).
+	// - AND the header's shifted byte value with the mask to get the bit
+	//   field value.
+	h.QR = msg[2] >> 7 & queryByteMask(1)
+	h.OpCode = OpCode(msg[2] >> 3 & queryByteMask(4))
+	h.AA = msg[2] >> 2 & queryByteMask(1)
+	h.TC = msg[2] >> 1 & queryByteMask(1)
+	h.RD = msg[2] >> 0 & queryByteMask(1)
+	h.RA = msg[3] >> 7 & queryByteMask(1)
+	h.RCode = RCode(msg[3] >> 0 & queryByteMask(4))
+
+	// The remaining bytes contain the remaining sections:
+	// - The 5th and 6th bytes contain the third section; QDCOUNT.
+	// - The 7th and 8th bytes contain the fourth section; ANCOUNT.
+	// - The 9th and 10th bytes contain the fifth section; NSCOUNT.
+	// - The 11th and 12th bytes contain the sixth section; ARCOUNT.
+	//
+	// Left-shift the first byte to the "left most" position, and OR it with the
+	// second byte to "merge" it back into a single section of 16 bits.
+	h.QDCount = uint16(msg[4])<<8 | uint16(msg[5])
+	h.ANCount = uint16(msg[6])<<8 | uint16(msg[7])
+	h.NSCount = uint16(msg[8])<<8 | uint16(msg[9])
+	h.ARCount = uint16(msg[10])<<8 | uint16(msg[11])
+
+	return len(msg), nil
+}
+
+// queryByteMask creates a bit mask where the "right most" n bits in a byte are
+// "turned on".
+//
+//   7   6   5   4   3   2   1   0
+// +---+---+---+---+---+---+---+---+
+// | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |	(1 << 0 ) - 1 = 0
+// +---+---+---+---+---+---+---+---+
+// | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 1 |	(1 << 1 ) - 1 = 1
+// +---+---+---+---+---+---+---+---+
+// | 0 | 0 | 0 | 0 | 0 | 0 | 1 | 1 |	(1 << 2 ) - 1 = 3
+// +---+---+---+---+---+---+---+---+
+// | 0 | 0 | 0 | 0 | 0 | 1 | 1 | 1 |	(1 << 3 ) - 1 = 7
+// +---+---+---+---+---+---+---+---+
+// | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |	(1 << 4 ) - 1 = 15
+// +---+---+---+---+---+---+---+---+
+// | 0 | 0 | 0 | 1 | 1 | 1 | 1 | 1 |	(1 << 5 ) - 1 = 31
+// +---+---+---+---+---+---+---+---+
+// | 0 | 0 | 1 | 1 | 1 | 1 | 1 | 1 |	(1 << 6 ) - 1 = 63
+// +---+---+---+---+---+---+---+---+
+// | 0 | 1 | 1 | 1 | 1 | 1 | 1 | 1 |	(1 << 7 ) - 1 = 127
+// +---+---+---+---+---+---+---+---+
+// | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 |	(1 << 8 ) - 1 = 255
+// +---+---+---+---+---+---+---+---+
+func queryByteMask(n int) byte {
+	return (1 << n) - 1
 }
---
+++ internal/dns/header_test.go
@@ -0,0 +1,87 @@
+package dns
+
+import "testing"
+
+func TestPackUnpack(t *testing.T) {
+	msg := Header{
+		ID:      1,
+		QR:      1,
+		OpCode:  OpCodeQuery,
+		AA:      1,
+		TC:      1,
+		RD:      1,
+		RA:      0,
+		Z:       0,
+		RCode:   RCodeNoError,
+		QDCount: 1,
+		ANCount: 2,
+		NSCount: 1,
+		ARCount: 4,
+	}
+
+	b, err := msg.Pack()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	h := new(Header)
+	lenb, err := h.Unpack(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if lenb != len(b) {
+		t.Errorf("unpacked bytes length error: got %v - want %v", lenb, len(b))
+	}
+
+	if h.ID != msg.ID {
+		t.Errorf("unpacked header ID error: got %v - want %v", h.ID, msg.ID)
+	}
+	if h.QR != msg.QR {
+		t.Errorf("unpacked header QR error: got %v - want %v", h.QR, msg.QR)
+	}
+	if h.OpCode != msg.OpCode {
+		t.Errorf(
+			"unpacked header OpCode error: got %v - want %v", h.OpCode, msg.OpCode,
+		)
+	}
+	if h.AA != msg.AA {
+		t.Errorf("unpacked header AA error: got %v - want %v", h.AA, msg.AA)
+	}
+	if h.TC != msg.TC {
+		t.Errorf("unpacked header TC error: got %v - want %v", h.TC, msg.TC)
+	}
+	if h.RD != msg.RD {
+		t.Errorf("unpacked header RD error: got %v - want %v", h.RD, msg.RD)
+	}
+	if h.RA != msg.RA {
+		t.Errorf("unpacked header RA error: got %v - want %v", h.RA, msg.RA)
+	}
+	if h.Z != msg.Z {
+		t.Errorf("unpacked header Z error: got %v - want %v", h.Z, msg.Z)
+	}
+	if h.RCode != msg.RCode {
+		t.Errorf(
+			"unpacked header RCode error: got %v - want %v", h.RCode, msg.RCode,
+		)
+	}
+	if h.QDCount != msg.QDCount {
+		t.Errorf(
+			"unpacked header QDCount error: got %v - want %v", h.QDCount, msg.QDCount,
+		)
+	}
+	if h.ANCount != msg.ANCount {
+		t.Errorf(
+			"unpacked header ANCount error: got %v - want %v", h.ANCount, msg.ANCount,
+		)
+	}
+	if h.NSCount != msg.NSCount {
+		t.Errorf(
+			"unpacked header NSCount error: got %v - want %v", h.NSCount, msg.NSCount,
+		)
+	}
+	if h.ARCount != msg.ARCount {
+		t.Errorf(
+			"unpacked header ARCount error: got %v - want %v", h.ARCount, msg.ARCount,
+		)
+	}
+}
